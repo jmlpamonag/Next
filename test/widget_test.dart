f3631f877955eaf9cefe51520bdd51f9dba88d31
Jyhdel Pamonag <jyjypamonag@gmail.com>
2022-12-15 22:01:39 -0500

    Add test case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,22 +3,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:next/src/app.dart';
 
 void main() {
-  testWidgets('Testing...', (WidgetTester tester) async {
+  testWidgets('Testing Home Screen...', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(App());
 
     // Verify that our counter starts at 0.
     expect(find.text('Next'), findsOneWidget);
     expect(find.byIcon(Icons.add), findsOneWidget);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle(Duration(milliseconds:400));
+  testWidgets('Testing Record Screen...', (WidgetTester tester) async {
+      // Navigate to Record Screen
+      await tester.pumpWidget(App());
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pumpAndSettle(Duration(milliseconds:400));
 
-    // Verify that we have the form by checking number of form fields
-    expect(find.byType(TextFormField), findsNWidgets(1));
-    expect(find.byType(TextField), findsNWidgets(5));
-
-
+      // Verify that we have the form by checking number of form fields
+      expect(find.byType(TextFormField), findsNWidgets(1));
+      expect(find.byType(TextField), findsAtLeastNWidgets(4));
   });
 }
